3dd54662d7a610c6a01bc61ee528aa19d8cdcc78
Tekle <74044761+chkheidzetekk@users.noreply.github.com>
2025-04-04 18:07:45 +0400

    Add files via upload

---
+++ Lecture_4.py
@@ -0,0 +1,32 @@
+# striq_count = 0
+# f = open('files.txt', 'r')
+# lines= f.readlines()
+#
+# for striq in  lines:
+#     striq_count +=1
+# f.close()
+#
+# print(striq_count)
+
+# # davaleba 1
+# striq_count = 0
+# with open('files.txt', 'r', encoding='utf-8') as f:
+#     lines = f.readlines()
+#
+#     for striq in lines:
+#         striq_count += 1
+#
+# print(striq_count)
+
+
+# # davaleba 2
+# sity_count = 0
+# f = open('files.txt', 'r', encoding='utf-8')
+# lines = f.readlines()
+#
+# for line in lines:
+#     sity_count += len(line.split())
+#
+# f.close()
+#
+# print(sity_count)
